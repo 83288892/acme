aa1e987bcb9b780107ad5d1c0ca66aa8ed0f47e5
li <43067679+83288892@users.noreply.github.com>
2023-08-01 19:49:37 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -1,192 +1,271 @@
 #!/bin/bash
 
-# 颜色代码
-BRIGHT_GREEN='\033[1;32m'
-YELLOW='\033[1;33m'
+# ANSI color codes
+GREEN='\033[0;32m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+# Function to check if a command exists
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
 
-# 检查依赖是否完整
-function check_dependencies() {
-    echo -e "${GREEN}正在检查依赖...${NC}"
-
-    # 添加检查依赖的逻辑，如检查 curl, sudo, socat, acme.sh 是否安装，未安装则提示用户进行安装
-    if ! command -v curl &> /dev/null || ! command -v sudo &> /dev/null || ! command -v socat &> /dev/null || ! command -v acme.sh &> /dev/null; then
-        echo -e "${RED}缺少必要工具，请安装依赖：curl, sudo, socat, acme.sh。"
-        echo -e "您可以使用以下命令安装 acme.sh："
-        echo -e "curl https://get.acme.sh | sh${NC}"
-        exit 1
+# Function to install acme.sh if not already installed
+install_acme_sh() {
+  if ! command_exists acme.sh; then
+    echo "Installing acme.sh..."
+    curl https://get.acme.sh | sh
+    if [ "$?" -ne 0 ]; then
+      echo -e "${RED}Failed to install acme.sh.${NC}"
+      exit 1
     fi
-
-    echo -e "${GREEN}依赖检查完成！${NC}"
+  fi
 }
 
-# 申请单域名证书
-function apply_single_domain_cert() {
-    read -p "请输入域名： " domain
-    # 对域名格式进行校验，避免用户输入错误格式
-    # ...
-
-    echo -e "${GREEN}正在申请证书，请稍等...${NC}"
-    # 调用 acme.sh 命令申请证书
-    ~/.acme.sh/acme.sh --register-account -m xxxx@gmail.com
-    ~/.acme.sh/acme.sh --issue -d "${domain}" --standalone
-    # ...
-    echo -e "${GREEN}证书申请成功！${NC}"
-}
-
-# 申请多域名证书
-function apply_multiple_domains_cert() {
-    read -p "请输入域名数量： " num_domains
-    # 对域名数量进行校验，确保为正整数
-    # ...
-
-    domains=()
-    for ((i=1; i<=num_domains; i++)); do
-        read -p "请输入第 $i 个域名： " domain
-        # 对域名格式进行校验，避免用户输入错误格式
-        # ...
-        domains+=("-d ${domain}")
-    done
-
-    echo -e "${GREEN}正在申请证书，请稍等...${NC}"
-    # 调用 acme.sh 命令申请证书
-    ~/.acme.sh/acme.sh --register-account -m xxxx@gmail.com --issue "${domains[@]}" --standalone
-    # ...
-    echo -e "${GREEN}证书申请成功！${NC}"
-}
-
-# 设置 Cloudflare API 密钥和邮箱
-function set_cf_api_key_email() {
-    read -p "请输入您的 Cloudflare API 密钥： " cf_key
-    read -p "请输入您的邮箱： " cf_email
-    # 对密钥和邮箱格式进行校验，避免错误输入
-    # ...
-    
-    export CF_Key="${cf_key}"
-    export CF_Email="${cf_email}"
-
-    echo -e "${GREEN}设置成功！${NC}"
-}
-
-# 申请 CF API 证书
-function apply_cf_api_cert() {
-    # 检查 CF API 密钥和邮箱是否设置
-    if [[ -z "$CF_Key" || -z "$CF_Email" ]]; then
-        echo -e "${RED}未设置 Cloudflare API 密钥或邮箱，请先设置${NC}"
-        set_cf_api_key_email
-    fi
-
-    read -p "请输入域名： " domain
-    # 对域名格式进行校验，避免用户输入错误格式
-    # ...
-
-    read -p "请输入泛域名（留空表示不申请泛域名）： " wildcard_domain
-    # 对泛域名格式进行校验，避免用户输入错误格式
-    # ...
-
-    echo -e "${GREEN}正在申请证书，请稍等...${NC}"
-    # 调用 acme.sh 命令申请证书
-    if [[ -z "$wildcard_domain" ]]; then
-        ~/.acme.sh/acme.sh --issue --dns dns_cf -d "${domain}"
+# Function to check if all dependencies are installed
+check_dependencies() {
+  echo "Checking dependencies..."
+  if ! command_exists curl || ! command_exists sudo || ! command_exists socat || ! command_exists acme.sh; then
+    echo -e "${RED}Some dependencies are missing. Please install: curl, sudo, socat, acme.sh${NC}"
+    read -p "Do you want to install acme.sh now? (y/n): " install_acme_sh_choice
+    if [ "$install_acme_sh_choice" = "y" ]; then
+      install_acme_sh
     else
-        ~/.acme.sh/acme.sh --issue --dns dns_cf -d "${domain}" -d "*.${wildcard_domain}"
+      exit 1
     fi
-    # ...
-    echo -e "${GREEN}证书申请成功！${NC}"
+  fi
 }
 
-# 完全卸载脚本并删除证书
-function uninstall_script() {
-    # 添加完全卸载脚本的逻辑，删除证书等
-    # ...
-    echo -e "${GREEN}脚本卸载完成！${NC}"
+# Function to validate domain name format
+validate_domain() {
+  # Regex pattern for domain name validation
+  domain_pattern="^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+  if [[ ! "$1" =~ $domain_pattern ]]; then
+    echo -e "${RED}Invalid domain format. Please enter a valid domain.${NC}"
+    return 1
+  fi
 }
 
-# 显示帮助信息
-function show_help() {
-    echo -e "${GREEN}脚本名称：cret.sh - 一键申请证书脚本"
-    echo -e "用法：cret.sh -c <menu_option>"
-    echo -e "可用菜单选项："
-    echo -e "  -c 1  检查依赖"
-    echo -e "  -c 2  一键申请证书"
-    echo -e "  -c 3  CF API 申请"
-    echo -e "  -c 4  完全卸载脚本并删除证书"
-    echo -e "  -c 5  显示帮助信息"
-    echo -e "  -c 6  退出脚本${NC}"
+# Function to register a single domain certificate
+register_single_domain_cert() {
+  read -p "Enter your email address: " email
+  read -p "Enter the domain name: " domain
+  validate_domain "$domain" || return 1
+  
+  echo "Registering account..."
+  ~/.acme.sh/acme.sh --register-account -m "$email"
+
+  echo "Issuing certificate for $domain..."
+  ~/.acme.sh/acme.sh --issue -d "$domain" --standalone
+  if [ "$?" -eq 0 ]; then
+    echo "Certificate issued successfully."
+    create_cert_dir "$domain"
+    copy_cert_files "$domain"
+  else
+    echo -e "${RED}Failed to issue certificate for $domain.${NC}"
+  fi
 }
 
-# 主菜单
-function main() {
-    if [[ "$#" -eq 0 ]]; then
-        echo -e "${RED}未提供命令行参数，请使用-c参数选择菜单选项${NC}"
-        show_help
-        exit 1
-    fi
+# Function to register a multi-domain certificate
+register_multi_domain_cert() {
+  read -p "Enter your email address: " email
+  read -p "Enter the number of domains: " num_domains
 
-    while getopts "c:h" opt; do
-        case "${opt}" in
-            c)
-                case "${OPTARG}" in
-                    1)
-                        check_dependencies
-                        ;;
-                    2)
-                        echo -e "请选择一键申请证书类型：\n[1] 单域名证书\n[2] 多域名证书"
-                        read -p "请选择： " cert_type
-                        case "${cert_type}" in
-                            1)
-                                apply_single_domain_cert
-                                ;;
-                            2)
-                                apply_multiple_domains_cert
-                                ;;
-                            *)
-                                echo -e "${RED}无效的选择！${NC}"
-                                ;;
-                        esac
-                        ;;
-                    3)
-                        echo -e "请选择 Cloudflare API 申请类型：\n[1] 设置 CF 密钥和邮箱\n[2] 申请证书"
-                        read -p "请选择： " cf_type
-                        case "${cf_type}" in
-                            1)
-                                set_cf_api_key_email
-                                ;;
-                            2)
-                                apply_cf_api_cert
-                                ;;
-                            *)
-                                echo -e "${RED}无效的选择！${NC}"
-                                ;;
-                        esac
-                        ;;
-                    4)
-                        uninstall_script
-                        ;;
-                    5)
-                        show_help
-                        ;;
-                    6)
-                        exit 0
-                        ;;
-                    *)
-                        echo -e "${RED}无效的菜单选项！${NC}"
-                        show_help
-                        ;;
-                esac
-                ;;
-            h)
-                show_help
-                ;;
-            *)
-                echo -e "${RED}无效的命令行参数！${NC}"
-                show_help
-                exit 1
-                ;;
-        esac
+  domains=()
+  for ((i = 1; i <= num_domains; i++)); do
+    read -p "Enter domain $i: " domain
+    validate_domain "$domain" || return 1
+    domains+=("-d" "$domain")
+  done
+
+  echo "Registering account..."
+  ~/.acme.sh/acme.sh --register-account -m "$email"
+
+  echo "Issuing multi-domain certificate..."
+  ~/.acme.sh/acme.sh --issue "${domains[@]}" --standalone
+  if [ "$?" -eq 0 ]; then
+    echo "Certificate issued successfully."
+    for domain in "${domains[@]:2}"; do
+      create_cert_dir "$domain"
+      copy_cert_files "$domain"
     done
+  else
+    echo -e "${RED}Failed to issue multi-domain certificate.${NC}"
+  fi
 }
 
-main "$@"
+# Function to create a directory for the certificate
+create_cert_dir() {
+  domain="$1"
+  cert_dir="/root/cert/$domain"
+  mkdir -p "$cert_dir"
+  echo "Certificate directory created: $cert_dir"
+}
+
+# Function to copy certificate files to the certificate directory
+copy_cert_files() {
+  domain="$1"
+  cert_dir="/root/cert/$domain"
+  ~/.acme.sh/acme.sh --install-cert -d "$domain" --cert-file "$cert_dir/cert.pem" \
+    --key-file "$cert_dir/key.pem" --fullchain-file "$cert_dir/fullchain.pem" \
+    --ca-file "$cert_dir/ca.pem"
+  echo "Certificate files copied to $cert_dir"
+}
+
+# Function to apply for a certificate using Cloudflare DNS API
+apply_with_cf_api() {
+  if [ -z "$CF_Key" ] || [ -z "$CF_Email" ]; then
+    echo -e "${RED}Cloudflare API key and email not set. Please set them using option [3-1].${NC}"
+    return 1
+  fi
+
+  read -p "Enter your domain name: " domain
+  read -p "Enter your wildcard domain (optional, leave empty if none): " wildcard_domain
+
+  if [ -n "$wildcard_domain" ]; then
+    validate_domain "$wildcard_domain" || return 1
+  fi
+  validate_domain "$domain" || return 1
+
+  domains=("-d" "$domain")
+  if [ -n "$wildcard_domain" ]; then
+    domains+=("-d" "*.$wildcard_domain")
+  fi
+
+  echo "Issuing certificate using Cloudflare DNS API..."
+  ~/.acme.sh/acme.sh --issue --dns dns_cf "${domains[@]}"
+  if [ "$?" -eq 0 ]; then
+    echo "Certificate issued successfully."
+    create_cert_dir "$domain"
+    copy_cert_files "$domain"
+    if [ -n "$wildcard_domain" ]; then
+      create_cert_dir "$wildcard_domain"
+      copy_cert_files "$wildcard_domain"
+    fi
+  else
+    echo -e "${RED}Failed to issue certificate using Cloudflare DNS API.${NC}"
+  fi
+}
+
+# Function to uninstall the script and delete certificates
+uninstall_script_and_certs() {
+  echo "Uninstalling the script and deleting certificates..."
+  rm -rf /root/cert
+  rm /usr/local/bin/cert.sh
+  echo "Script and certificates successfully uninstalled."
+}
+
+# Function to display help information
+display_help() {
+  echo "Usage: cert.sh [option]"
+  echo "Options:"
+  echo "  [1] Check Dependencies"
+  echo "  [2] One-Click Application"
+  echo "  [3] CF_API Application"
+  echo "  [4] Uninstall Script and Certificates"
+  echo "  [5] Help"
+  echo "  [6] Exit"
+}
+
+# Function to display the main menu
+display_main_menu() {
+  echo "-------------------------------------"
+  echo "Welcome to Cert.sh - One-Click Certificate Application"
+  echo "-------------------------------------"
+  echo "Main Menu:"
+  echo "[1] Check Dependencies"
+  echo "[2] One-Click Application"
+  echo "    [2-1] Single Domain Certificate"
+  echo "    [2-2] Multi-Domain Certificate"
+  echo "[3] CF_API Application"
+  echo "    [3-1] Set Cloudflare API Key and Email"
+  echo "    [3-2] Apply for Certificate with CF_API"
+  echo "[4] Uninstall Script and Certificates"
+  echo "[5] Help"
+  echo "[6] Exit"
+  echo "-------------------------------------"
+}
+
+# Main script logic
+main() {
+  check_dependencies
+
+  while true; do
+    display_main_menu
+    read -p "Enter your choice: " choice
+
+    case "$choice" in
+      1)
+        echo "Checking dependencies..."
+        check_dependencies
+        ;;
+      2)
+        while true; do
+          echo "Certificate Application Menu:"
+          echo "[2-1] Single Domain Certificate"
+          echo "[2-2] Multi-Domain Certificate"
+          echo "[9] Back to Main Menu"
+          read -p "Enter your choice: " cert_choice
+
+          case "$cert_choice" in
+            2-1)
+              register_single_domain_cert
+              ;;
+            2-2)
+              register_multi_domain_cert
+              ;;
+            9)
+              break
+              ;;
+            *)
+              echo -e "${RED}Invalid choice. Please try again.${NC}"
+              ;;
+          esac
+        done
+        ;;
+      3)
+        while true; do
+          echo "CF_API Application Menu:"
+          echo "[3-1] Set Cloudflare API Key and Email"
+          echo "[3-2] Apply for Certificate with CF_API"
+          echo "[9] Back to Main Menu"
+          read -p "Enter your choice: " cf_choice
+
+          case "$cf_choice" in
+            3-1)
+              read -p "Enter your Cloudflare API Key: " CF_Key
+              read -p "Enter your Cloudflare email: " CF_Email
+              export CF_Key CF_Email
+              echo "Cloudflare API Key and Email set successfully."
+              ;;
+            3-2)
+              apply_with_cf_api
+              ;;
+            9)
+              break
+              ;;
+            *)
+              echo -e "${RED}Invalid choice. Please try again.${NC}"
+              ;;
+          esac
+        done
+        ;;
+      4)
+        uninstall_script_and_certs
+        exit 0
+        ;;
+      5)
+        display_help
+        ;;
+      6)
+        echo "Exiting Cert.sh..."
+        exit 0
+        ;;
+      *)
+        echo -e "${RED}Invalid choice. Please try again.${NC}"
+        ;;
+    esac
+  done
+}
+
+main
