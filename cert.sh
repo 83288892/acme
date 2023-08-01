3eab46e41e858b38dc1c4ffb1ce64afdba89c67f
li <43067679+83288892@users.noreply.github.com>
2023-08-01 13:18:57 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -1,56 +1,53 @@
 #!/bin/bash
 
-# 设置提示信息颜色
+# 设置颜色变量
 GREEN='\033[1;32m'
 NC='\033[0m'
 
-# 检查是否以root权限运行
+# 检查是否为root用户运行脚本
 if [[ $EUID -ne 0 ]]; then
-    echo -e "${GREEN}请使用root权限运行此脚本.${NC}"
+    echo -e "${GREEN}请使用root权限运行该脚本.${NC}"
     exit 1
 fi
 
-# 安装所需软件包
-echo -e "${GREEN}正在安装所需软件包（acme, socat）...${NC}"
+# 安装acme和socat
+echo -e "${GREEN}开始安装 acme 和 socat.${NC}"
 apt-get update
 apt-get install -y acme socat
 
-# 输入Cloudflare密钥和邮箱
-echo -e "${GREEN}请输入您的Cloudflare API密钥:${NC}"
-read CF_API_KEY
-echo -e "${GREEN}请输入您的Cloudflare邮箱:${NC}"
-read CF_EMAIL
+# 获取Cloudflare密钥和邮箱
+echo -e "${GREEN}请输入Cloudflare邮箱和密钥.${NC}"
+read -p "邮箱: " CF_EMAIL
+read -p "密钥: " CF_KEY
 
 # 验证Cloudflare密钥和邮箱
-echo -e "${GREEN}正在验证Cloudflare密钥和邮箱...${NC}"
-if ! acme.sh --issue --dns dns_cf -d example.com -d '*.example.com' --accountemail "${CF_EMAIL}" --dns_cf_key "${CF_API_KEY}"; then
-    echo -e "${GREEN}验证Cloudflare密钥和邮箱失败，请检查您的API密钥和邮箱.${NC}"
+echo -e "${GREEN}验证Cloudflare密钥和邮箱.${NC}"
+if ! acme.sh --issue --dns dns_cf -d example.com -d '*.example.com' --keypath /root/.acme.sh/account.key --email "${CF_EMAIL}" --dns_cf_key "${CF_KEY}"; then
+    echo -e "${GREEN}Cloudflare密钥或邮箱验证失败，请检查输入是否正确.${NC}"
     exit 1
 fi
 
-# 输入域名并申请证书
-echo -e "${GREEN}请输入您的域名（例如，example.com）:${NC}"
-read DOMAIN
-echo -e "${GREEN}是否申请泛域名证书？（是/否）:${NC}"
-read WILDCARD
+# 输入域名和泛域名
+echo -e "${GREEN}请输入要申请证书的域名和泛域名（多个域名用空格分隔）.${NC}"
+read -p "域名: " DOMAIN
+read -p "泛域名: " WILDCARD_DOMAIN
 
-if [ "$WILDCARD" = "是" ]; then
-    acme.sh --issue --dns dns_cf -d "$DOMAIN" -d "*.$DOMAIN" --accountemail "${CF_EMAIL}" --dns_cf_key "${CF_API_KEY}"
-else
-    acme.sh --issue --dns dns_cf -d "$DOMAIN" --accountemail "${CF_EMAIL}" --dns_cf_key "${CF_API_KEY}"
+# 申请证书
+echo -e "${GREEN}开始申请证书.${NC}"
+if ! acme.sh --issue --dns dns_cf -d "${DOMAIN}" -d "*.${WILDCARD_DOMAIN}" --keypath /root/.acme.sh/account.key --email "${CF_EMAIL}" --dns_cf_key "${CF_KEY}"; then
+    echo -e "${GREEN}证书申请失败，请检查输入是否正确.${NC}"
+    exit 1
 fi
 
-# 检查证书目录是否存在，不存在则创建
+# 检查证书目录是否存在，如果不存在则创建
 CERT_DIR="/root/cert"
 if [ ! -d "$CERT_DIR" ]; then
+    echo -e "${GREEN}创建证书目录: $CERT_DIR.${NC}"
     mkdir -p "$CERT_DIR"
 fi
 
-# 将证书文件复制到证书目录
-if [ "$WILDCARD" = "是" ]; then
-    acme.sh --install-cert -d "$DOMAIN" --key-file "$CERT_DIR/$DOMAIN.key" --fullchain-file "$CERT_DIR/fullchain.cer"
-else
-    acme.sh --install-cert -d "$DOMAIN" --key-file "$CERT_DIR/$DOMAIN.key" --cert-file "$CERT_DIR/$DOMAIN.cer" --ca-file "$CERT_DIR/ca.cer" --fullchain-file "$CERT_DIR/fullchain.cer"
-fi
+# 复制证书到证书目录
+echo -e "${GREEN}复制证书到 $CERT_DIR 目录.${NC}"
+acme.sh --install-cert -d "${DOMAIN}" -d "*.${WILDCARD_DOMAIN}" --cert-file "$CERT_DIR/cert.pem" --key-file "$CERT_DIR/private.key" --fullchain-file "$CERT_DIR/fullchain.pem"
 
-echo -e "${GREEN}证书申请成功，并已复制到$CERT_DIR目录.${NC}"
+echo -e "${GREEN}证书申请成功！证书已保存在 $CERT_DIR 目录.${NC}"
