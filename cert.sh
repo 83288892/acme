eb6ee0a64b5d1ef5600f31c6500cc8c456721228
li <43067679+83288892@users.noreply.github.com>
2023-08-01 16:53:19 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -2,14 +2,15 @@
 
 # 设置颜色变量
 GREEN='\033[1;32m'
+RED='\033[1;31m'
 NC='\033[0m' # No Color
 
-# 加载 acme.sh 路径
-export PATH="$HOME/.acme.sh:$PATH"
+# 证书保存路径
+CERT_PATH="/root/cert"
 
 # 函数：安装依赖 acme.sh 和 socat
 install_dependencies() {
-    echo -e "${GREEN}正在安装 acme.sh 和 socat...${NC}"
+    echo -e "${GREEN}正在安装 acme.sh 和 socat，请稍等...${NC}"
     apt-get update
     apt-get install -y socat
     curl https://get.acme.sh | sh
@@ -24,12 +25,12 @@ input_cloudflare_api() {
 
 # 函数：验证 Cloudflare API 密钥和邮箱
 verify_cloudflare_api() {
-    echo -e "${GREEN}验证 Cloudflare API 密钥和邮箱...${NC}"
+    echo -e "${GREEN}验证 Cloudflare API 密钥和邮箱，请稍等...${NC}"
     acme.sh --set-default-ca --server letsencrypt --dns dns_cf --accountemail $CF_EMAIL --accountkey $CF_API_KEY --test
 
     # 验证密钥和电子邮件的有效性
     if [ $? -ne 0 ]; then
-        echo -e "${GREEN}API 密钥或电子邮件验证失败，请检查输入的信息。返回主菜单。${NC}"
+        echo -e "${RED}API 密钥或电子邮件验证失败，请检查输入的信息。返回主菜单。${NC}"
         return 1
     else
         echo -e "${GREEN}API 密钥和电子邮件验证成功。${NC}"
@@ -44,7 +45,8 @@ apply_certificate() {
         echo -e "  [1]主域名"
         echo -e "  [2]单域名"
         echo -e "  [3]泛域名"
-        echo -e "  [4]返回主菜单"
+        echo -e "  [4]重新配置 Cloudflare API 密钥和电子邮件"
+        echo -e "  [5]退出脚本"
 
         read -r -t 0
         read -p "请输入选项编号: " domain_type
@@ -53,88 +55,137 @@ apply_certificate() {
             1|2|3)
                 if [ $domain_type -eq 1 ]; then
                     read -p "请输入主域名: " main_domain
+                    validate_domain_format "$main_domain" || continue
                 elif [ $domain_type -eq 2 ]; then
                     read -p "请输入单域名: " single_domain
+                    validate_domain_format "$single_domain" || continue
                 elif [ $domain_type -eq 3 ]; then
                     read -p "请输入泛域名: " wildcard_domain
+                    validate_domain_format "$wildcard_domain" || continue
                 fi
 
-                # 验证 Cloudflare API 密钥和邮箱
-                verify_cloudflare_api || return 1
+                if [ $domain_type -eq 4 ]; then
+                    input_cloudflare_api
+                    verify_cloudflare_api || continue
+                fi
 
                 # 申请证书
-                acme.sh --issue --dns dns_cf -d ${main_domain:-${single_domain:-$wildcard_domain}} --key-file $cert_path/${main_domain:-${single_domain:-$wildcard_domain}}.key --fullchain-file $cert_path/${main_domain:-${single_domain:-$wildcard_domain}}.cer --keylength ec-256 --force
+                acme.sh --issue --dns dns_cf -d ${main_domain:-${single_domain:-$wildcard_domain}} --key-file $CERT_PATH/${main_domain:-${single_domain:-$wildcard_domain}}.key --fullchain-file $CERT_PATH/${main_domain:-${single_domain:-$wildcard_domain}}.cer --keylength ec-256 --force
 
                 if [ $? -eq 0 ]; then
                     copy_certificate
-                    echo -e "${GREEN}证书申请成功并已复制到目录 $cert_path${NC}"
+                    echo -e "${GREEN}证书申请成功并已复制到目录 $CERT_PATH${NC}"
                     break
                 else
-                    echo -e "${GREEN}证书申请失败，请检查错误信息。脚本将重新开始申请。${NC}"
+                    echo -e "${RED}证书申请失败，请检查错误信息。脚本将重新开始申请。${NC}"
                 fi
                 ;;
-            4)
-                echo -e "${GREEN}返回主菜单。${NC}"
-                break
+            5)
+                echo -e "${GREEN}退出脚本。${NC}"
+                exit 0
                 ;;
             *)
-                echo -e "${GREEN}无效的选择。${NC}"
+                echo -e "${RED}无效的选择，请重新输入。${NC}"
                 ;;
         esac
     done
 }
 
+# 函数：验证域名格式
+validate_domain_format() {
+    domain=$1
+    if ! [[ "$domain" =~ ^([a-zA-Z0-9.-]+\.)+[a-zA-Z]{2,}$ ]]; then
+        echo -e "${RED}无效的域名格式，请重新输入。${NC}"
+        return 1
+    fi
+    return 0
+}
+
 # 函数：检查证书路径并创建
 create_cert_directory() {
-    cert_path="/root/cert"
-    if [ ! -d $cert_path ]; then
-        mkdir -p $cert_path
+    if [ ! -d $CERT_PATH ]; then
+        mkdir -p $CERT_PATH
     fi
 }
 
 # 函数：复制证书到指定目录
 copy_certificate() {
     domain_name="${main_domain:-${single_domain:-$wildcard_domain}}"
-    cp "$HOME/.acme.sh/$domain_name"_ecc/*.cer $cert_path/
-    cp "$HOME/.acme.sh/$domain_name"_ecc/*.key $cert_path/
+    cp "$HOME/.acme.sh/$domain_name"_ecc/*.cer $CERT_PATH/
+    cp "$HOME/.acme.sh/$domain_name"_ecc/*.key $CERT_PATH/
 }
 
 # 函数：显示简介
 display_intro() {
     echo -e "${GREEN}脚本简介：${NC}"
     echo -e "${GREEN}本脚本可用于一键申请证书并将证书复制到指定目录。${NC}"
-    echo -e "${GREEN}在开始执行脚本之前，请确保您已安装了acme.sh和socat，且已获取Cloudflare API密钥和电子邮件。${NC}"
+    echo -e "${GREEN}在开始执行脚本之前，请确保已获取Cloudflare API密钥和电子邮件。${NC}"
 }
 
+# 函数：显示帮助信息
+display_help() {
+    echo -e "${GREEN}用法: $0 [选项]${NC}"
+    echo -e "${GREEN}选项："
+    echo -e "  -h, --help  显示帮助信息"
+    echo -e "  -p, --path  指定证书保存路径，默认为 /root/cert${NC}"
+}
+
+# 自动加载 acme.sh 路径
+export PATH="$HOME/.acme.sh:$PATH"
 
 # 主函数
 main() {
+    while [ $# -gt 0 ]; do
+        case $1 in
+            -h|--help)
+                display_help
+                exit 0
+                ;;
+            -p|--path)
+                if [ -z "$2" ]; then
+                    echo -e "${RED}未指定证书保存路径。${NC}"
+                    exit 1
+                else
+                    CERT_PATH="$2"
+                    shift
+                fi
+                ;;
+            *)
+                echo -e "${RED}无效的选项：$1。${NC}"
+                exit 1
+                ;;
+        esac
+        shift
+    done
+
     display_intro
 
     while true; do
         echo -e "${GREEN}请选择操作：${NC}"
-        echo -e "  [1]申请证书"
-        echo -e "  [2]退出脚本"
+        echo -e "  [1]安装依赖 acme.sh 和 socat"
+        echo -e "  [2]申请证书"
+        echo -e "  [3]退出脚本"
 
         read -r -t 0
         read -p "请输入选项编号: " menu_choice
 
         case $menu_choice in
             1)
-                input_cloudflare_api
-                verify_cloudflare_api || continue
+                install_dependencies
+                ;;
+            2)
                 create_cert_directory
                 apply_certificate
                 ;;
-            2)
+            3)
                 echo -e "${GREEN}退出脚本。${NC}"
                 exit 0
                 ;;
             *)
-                echo -e "${GREEN}无效的选择。${NC}"
+                echo -e "${RED}无效的选择，请重新输入。${NC}"
                 ;;
         esac
     done
 }
 
-main
+main "$@"
