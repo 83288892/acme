bf9222a5cf5cfb11ae59843fe2102ab943c5ad12
li <43067679+83288892@users.noreply.github.com>
2023-08-01 14:12:33 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -1,57 +1,54 @@
 #!/bin/bash
 
-# 设置脚本全部提示显示字体为高亮大绿色
-echo -e "\e[1;32m"
+# 设置高亮大绿色字体
+echo -e "\033[1;32m" 
 
-# 检查本地是否安装有acme和socat
-if ! command -v acme.sh &>/dev/null; then
-  echo "acme.sh 未安装，正在安装..."
-  apt-get install acme.sh
+# 检查 acme.sh 和 socat 是否已安装
+if [ ! -f "/root/.acme.sh/acme.sh" ] || [ ! -f "/usr/bin/socat" ]; then
+  echo -e "\033[1;32macme.sh 或 socat 未安装,开始安装...\033[0m"
+  apt install socat -y
+  curl https://get.acme.sh | sh
+else
+  echo -e "\033[1;32macme.sh 和 socat 已安装,开始申请证书\033[0m"
 fi
 
-if ! command -v socat &>/dev/null; then
-  echo "socat 未安装，正在安装..."
-  apt-get install socat
-fi
+# 输入 API 密钥和邮箱,并验证
+while : 
+do
+  read -p "请输入 Cloudflare API 密钥:" CF_Key
+  read -p "请输入 Cloudflare 邮箱:" CF_Email
+  CF_Key="${CF_Key//[[:space:]]/}" # 去除空格
+  CF_Email="${CF_Email//[[:space:]]/}"
+  
+  if [ -z "$CF_Key" ] || [ -z "$CF_Email" ]; then
+    echo -e "\033[1;31m密钥或邮箱不能为空!\033[0m"
+  else
+    break
+  fi  
+done
 
-# 输入cloudflareAPI密钥和cloudflare邮箱
-echo "请输入cloudflareAPI密钥："
-read CF_API_KEY
-echo "请输入cloudflare邮箱："
-read CF_EMAIL
-
-# 在系统中配置Cloudflare的API密钥和电子邮件
-echo "CLOUDFLARE_API_KEY=\"$CF_API_KEY\" > /etc/cloudflare.conf"
-echo "CLOUDFLARE_EMAIL=\"$CF_EMAIL\" >> /etc/cloudflare.conf"
-
-# 验证密钥和电子邮件的有效性
-cf api -a $CF_API_KEY -u $CF_EMAIL
-
-# 提示输入域名和泛域名
-echo "请输入域名："
-read DOMAIN
-echo "请输入泛域名："
-read WILDCARD_DOMAIN
-
-# 申请证书
-echo "正在申请证书..."
-acme.sh --issue --dns cloudflare -d $DOMAIN -d $WILDCARD_DOMAIN
-
-# 申请成功后检测本地是否有证书路径/root/cert
-if [[ ! -d /root/cert ]]; then
-  echo "证书路径/root/cert不存在，正在创建..."
-  mkdir /root/cert
-fi
-
-# 最后复制证书到/root/cert目录
-cp /etc/letsencrypt/live/$DOMAIN/fullchain.pem /root/cert
-cp /etc/letsencrypt/live/$DOMAIN/privkey.pem /root/cert
-
-# 申请失败提示失败信息并退出脚本
-if [[ $? -ne 0 ]]; then
-  echo "证书申请失败，请检查您的域名和cloudflareAPI密钥"
+# 配置 Cloudflare API 和验证可用性
+~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+if ~/.acme.sh/acme.sh  --register-account  -m $CF_Email --server letsencrypt --accountemail $CF_Email --accountkey $CF_Key; then
+  echo -e "\033[1;32mCloudflare API 验证成功!\033[0m" 
+else
+  echo -e "\033[1;31mCloudflare API 验证失败,请检查后重试!\033[0m"
   exit 1
 fi
 
-# 提示证书申请成功
-echo "证书申请成功！证书已保存在/root/cert目录下"
+# 输入域名并申请证书
+read -p "请输入要申请证书的域名:" domain
+if ~/.acme.sh/acme.sh --issue --dns dns_cf -d $domain --keylength ec-256 --pre-hook "systemctl stop nginx" --post-hook "systemctl restart nginx"; then
+  echo -e "\033[1;32m证书申请成功!\033[0m"
+else
+  echo -e "\033[1;31m证书申请失败,请重试!\033[0m"
+  exit 1 
+fi
+
+# 检查证书目录并复制证书
+cert_path="/root/cert"
+if [ ! -d "$cert_path" ]; then
+  mkdir "$cert_path"
+fi
+~/.acme.sh/acme.sh --install-cert -d $domain --cert-file $cert_path/cert.pem --key-file $cert_path/key.pem --fullchain-file $cert_path/fullchain.pem --reloadcmd "systemctl restart nginx"
+echo -e "\033[1;32m证书已复制到 $cert_path 目录!\033[0m"
