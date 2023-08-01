61387128b0a7e1e803a837278b82261d1f6e17af
li <43067679+83288892@users.noreply.github.com>
2023-08-01 16:08:30 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -2,91 +2,109 @@
 
 # 设置颜色变量
 GREEN='\033[1;32m'
-RED='\033[0;31m'
-NC='\033[0m' 
+NC='\033[0m' # No Color
 
-# 加载acme.sh路径
-export PATH="$HOME/.acme.sh:$PATH"  
+# 加载 acme.sh 路径
+export PATH="$HOME/.acme.sh:$PATH"
 
-# 定义函数安装依赖
-install_dep() {
-  if ! command -v $1 &> /dev/null; then
-    echo -e "${GREEN}Installing $1${NC}"
-    if [ $1 = "acme.sh" ]; then
-      curl https://get.acme.sh | sh
-    elif [ $1 = "socat" ]; then  
-      apt-get install -y socat
+# 函数：安装依赖 acme.sh 和 socat
+install_dependencies() {
+    echo -e "${GREEN}正在安装 acme.sh 和 socat...${NC}"
+    apt-get update
+    apt-get install -y socat
+    curl https://get.acme.sh | sh
+}
+
+# 函数：输入 Cloudflare API 密钥和邮箱
+input_cloudflare_api() {
+    echo -e "${GREEN}请输入 Cloudflare API 密钥和电子邮件：${NC}"
+    read -p "Cloudflare API 密钥: " CF_API_KEY
+    read -p "Cloudflare 邮箱: " CF_EMAIL
+}
+
+# 函数：配置 Cloudflare 的 API 密钥和电子邮件
+configure_cloudflare_api() {
+    echo -e "${GREEN}配置 Cloudflare 的 API 密钥和电子邮件...${NC}"
+    acme.sh --set-default-ca --server letsencrypt --dns dns_cf --accountemail $CF_EMAIL --accountkey $CF_API_KEY
+
+    # 验证密钥和电子邮件的有效性
+    if [ $? -ne 0 ]; then
+        echo -e "${GREEN}API 密钥或电子邮件验证失败，请检查输入的信息。脚本将退出。${NC}"
+        exit 1
     fi
-  fi
 }
 
-# 检查并安装依赖
-install_dep acme.sh
-install_dep socat
+# 函数：选择并申请证书
+apply_certificate() {
+    case $domain_type in
+        1)
+            read -p "请输入主域名: " main_domain
+            ;;
+        2)
+            read -p "请输入单域名: " single_domain
+            ;;
+        3)
+            read -p "请输入泛域名: " wildcard_domain
+            ;;
+        4)
+            echo -e "${GREEN}退出脚本。${NC}"
+            exit 0
+            ;;
+        *)
+            echo -e "${GREEN}无效的选择。脚本将退出。${NC}"
+            exit 1
+            ;;
+    esac
 
-# 输入API信息
-echo -e "${GREEN}请输入Cloudflare API密钥和邮箱:${NC}"
-read -p "API密钥:" CF_API_KEY
-read -p "邮箱:" CF_EMAIL
-
-# 验证API 
-verify_api() {
-  echo -e "${GREEN}正在验证API信息...${NC}" 
-  if ! acme.sh --set-default-ca --server letsencrypt --dns dns_cf --accountemail $CF_EMAIL --accountkey $CF_API_KEY; then
-    echo -e "${RED}API验证失败,请检查信息${NC}"
-    exit 1
-  fi
+    # 申请证书
+    acme.sh --issue --dns dns_cf -d ${main_domain:-${single_domain:-$wildcard_domain}} --key-file $cert_path/${main_domain:-${single_domain:-$wildcard_domain}}.key --fullchain-file $cert_path/${main_domain:-${single_domain:-$wildcard_domain}}.cer --keylength ec-256 --force
 }
 
-verify_api
-
-# 定义证书保存路径  
-CERT_PATH="/root/cert"
-
-# 申请证书函数
-issue_cert() {
-  domain=$1
-  echo -e "${GREEN}开始为$domain申请证书${NC}"
-  
-  if ! acme.sh --issue --dns dns_cf -d $domain --key-file $CERT_PATH/$domain.key --fullchain-file $CERT_PATH/$domain.cer --keylength ec-256 --force; then
-    echo -e "${RED}申请证书失败,错误信息:${NC}" && acme.sh --issue --dns dns_cf -d $domain --key-file $CERT_PATH/$domain.key --fullchain-file $CERT_PATH/$domain.cer --keylength ec-256 --force --debug
-  fi
-
-  echo -e "${GREEN}证书申请成功${NC}"
+# 函数：检查证书路径并创建
+create_cert_directory() {
+    cert_path="/root/cert"
+    if [ ! -d $cert_path ]; then
+        mkdir -p $cert_path
+    fi
 }
 
-# 输入域名信息
-while :; do
-  echo -e "${GREEN}请输入域名类型:${NC}"
-  read -p "[1] 主域名 [2] 单域名 [3] 泛域名 [其他退出]" input
-  
-  case $input in
-  1)
-    read -p "请输入主域名:" main_domain
-    issue_cert $main_domain
-    break
-    ;;
-  2)
-    read -p "请输入单域名:" single_domain  
-    issue_cert $single_domain
-    break
-    ;;
-  3)
-    read -p "请输入泛域名:" wildcard_domain 
-    issue_cert *.$wildcard_domain
-    break
-    ;;
-  *)
-    echo -e "${RED}输入有误,退出脚本${NC}"
-    exit 1
-    ;;
-  esac
-done
+# 函数：复制证书到指定目录
+copy_certificate() {
+    domain_name="${main_domain:-${single_domain:-$wildcard_domain}}"
+    cp "$HOME/.acme.sh/$domain_name"_ecc/*.cer $cert_path/
+    cp "$HOME/.acme.sh/$domain_name"_ecc/*.key $cert_path/
+}
 
-# 检查证书目录
-if [ ! -d "$CERT_PATH" ]; then
-  mkdir -p "$CERT_PATH" 
-fi
+# 主函数
+main() {
+    install_dependencies
 
-# 完成提示
-echo -e "${GREEN}证书申请完成,已保存到$CERT_PATH${NC}"
+    input_cloudflare_api
+
+    configure_cloudflare_api
+
+    while true; do
+        echo -e "${GREEN}请选择需要申请的域名类型：${NC}"
+        echo -e "  [1]主域名"
+        echo -e "  [2]单域名"
+        echo -e "  [3]泛域名"
+        echo -e "  [4]退出脚本"
+
+        read -p "请输入选项编号: " domain_type
+
+        echo -e "${GREEN}开始申请证书...${NC}"
+        create_cert_directory
+
+        apply_certificate
+
+        if [ $? -eq 0 ]; then
+            copy_certificate
+            echo -e "${GREEN}证书申请成功并已复制到目录 $cert_path${NC}"
+            break
+        else
+            echo -e "${GREEN}证书申请失败，请检查错误信息。脚本将重新开始申请。${NC}"
+        fi
+    done
+}
+
+main
