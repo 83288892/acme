488e56c1fbe69b592fa2a900bcad918fd8ac063a
li <43067679+83288892@users.noreply.github.com>
2023-08-01 13:11:42 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -1,50 +1,56 @@
 #!/bin/bash
 
-# 设置提示文字样式
-GREEN="\033[1;32m"
-RESET="\033[0m"
+# 设置提示信息颜色
+GREEN='\033[1;32m'
+NC='\033[0m'
 
-# 安装 acme 和 socat
-echo -e "${GREEN}开始安装 acme 和 socat...${RESET}"
+# 检查是否以root权限运行
+if [[ $EUID -ne 0 ]]; then
+    echo -e "${GREEN}请使用root权限运行此脚本.${NC}"
+    exit 1
+fi
+
+# 安装所需软件包
+echo -e "${GREEN}正在安装所需软件包（acme, socat）...${NC}"
 apt-get update
 apt-get install -y acme socat
 
-# 输入密钥和邮箱
-read -p "请输入您的密钥: " KEY
-read -p "请输入您的邮箱: " EMAIL
+# 输入Cloudflare密钥和邮箱
+echo -e "${GREEN}请输入您的Cloudflare API密钥:${NC}"
+read -s CF_API_KEY
+echo -e "${GREEN}请输入您的Cloudflare邮箱:${NC}"
+read CF_EMAIL
 
-# 验证密钥和邮箱
-echo -e "${GREEN}开始验证密钥和邮箱...${RESET}"
-# 这里省略验证的步骤，你可以根据具体的流程和工具来验证密钥和邮箱
-
-# Cloudflare API相关信息
-CLOUDFLARE_API_KEY="YOUR_CLOUDFLARE_API_KEY"
-CLOUDFLARE_EMAIL="YOUR_CLOUDFLARE_EMAIL"
-
-# 提示输入域名和泛域名
-read -p "请输入域名: " DOMAIN
-read -p "请输入泛域名(如果没有可留空): " WILDCARD
-
-# 使用Cloudflare API验证域名所有权
-echo -e "${GREEN}使用Cloudflare API验证域名所有权...${RESET}"
-if [ -n "$WILDCARD" ]; then
-  # 如果有泛域名，使用通配符验证
-  acme.sh --issue --dns dns_cf -d "$DOMAIN" -d "*.$WILDCARD" --keypath "/root/.acme.sh/$DOMAIN/$DOMAIN.key" --fullchainpath "/root/.acme.sh/$DOMAIN/fullchain.cer"
-else
-  # 没有泛域名，只验证单个域名
-  acme.sh --issue --dns dns_cf -d "$DOMAIN" --keypath "/root/.acme.sh/$DOMAIN/$DOMAIN.key" --fullchainpath "/root/.acme.sh/$DOMAIN/fullchain.cer"
+# 验证Cloudflare密钥和邮箱
+echo -e "${GREEN}正在验证Cloudflare密钥和邮箱...${NC}"
+if ! acme.sh --issue --dns dns_cf -d example.com -d '*.example.com' --accountemail "${CF_EMAIL}" --dns_cf_key "${CF_API_KEY}"; then
+    echo -e "${GREEN}验证Cloudflare密钥和邮箱失败，请检查您的API密钥和邮箱.${NC}"
+    exit 1
 fi
 
-# 检查证书路径，如果不存在就创建
+# 输入域名并申请证书
+echo -e "${GREEN}请输入您的域名（例如，example.com）:${NC}"
+read DOMAIN
+echo -e "${GREEN}是否申请泛域名证书？（是/否）:${NC}"
+read WILDCARD
+
+if [ "$WILDCARD" = "是" ]; then
+    acme.sh --issue --dns dns_cf -d "$DOMAIN" -d "*.$DOMAIN" --accountemail "${CF_EMAIL}" --dns_cf_key "${CF_API_KEY}"
+else
+    acme.sh --issue --dns dns_cf -d "$DOMAIN" --accountemail "${CF_EMAIL}" --dns_cf_key "${CF_API_KEY}"
+fi
+
+# 检查证书目录是否存在，不存在则创建
 CERT_DIR="/root/cert"
 if [ ! -d "$CERT_DIR" ]; then
-  echo -e "${GREEN}创建证书路径：${CERT_DIR}...${RESET}"
-  mkdir -p "$CERT_DIR"
+    mkdir -p "$CERT_DIR"
 fi
 
-# 复制证书到指定目录
-echo -e "${GREEN}复制证书到${CERT_DIR}目录...${RESET}"
-cp "/root/.acme.sh/$DOMAIN/$DOMAIN.key" "$CERT_DIR/"
-cp "/root/.acme.sh/$DOMAIN/fullchain.cer" "$CERT_DIR/"
+# 将证书文件复制到证书目录
+if [ "$WILDCARD" = "是" ]; then
+    acme.sh --install-cert -d "$DOMAIN" --key-file "$CERT_DIR/$DOMAIN.key" --fullchain-file "$CERT_DIR/fullchain.cer"
+else
+    acme.sh --install-cert -d "$DOMAIN" --key-file "$CERT_DIR/$DOMAIN.key" --cert-file "$CERT_DIR/$DOMAIN.cer" --ca-file "$CERT_DIR/ca.cer" --fullchain-file "$CERT_DIR/fullchain.cer"
+fi
 
-echo -e "${GREEN}证书申请和复制过程已完成！${RESET}"
+echo -e "${GREEN}证书申请成功，并已复制到$CERT_DIR目录.${NC}"
