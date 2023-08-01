f5fbaec5b8b84b7b6eb087c7112aa8fe1bab36a6
li <43067679+83288892@users.noreply.github.com>
2023-08-01 15:40:51 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -1,45 +1,64 @@
 #!/bin/bash
 
-# 字体高亮 大绿色
-echo -e "\033[1;32m"
+# 设置颜色变量
+GREEN='\033[1;32m'
+NC='\033[0m' # No Color
 
-# 检查工具依赖
-if ! command -v acme.sh >/dev/null 2>&1; then
-  curl https://get.acme.sh | sh
-  export PATH=$PATH:~/.acme.sh 
+# 检查是否安装acme和socat
+if ! command -v acme.sh &> /dev/null || ! command -v socat &> /dev/null; then
+    echo -e "${GREEN}正在安装acme.sh和socat...${NC}"
+    apt-get update
+    apt-get install -y socat
+    curl https://get.acme.sh | sh
 fi
 
-if ! command -v socat >/dev/null 2>&1; then
-  sudo apt install -y socat
+# 输入Cloudflare API密钥和邮箱
+echo -e "${GREEN}请输入Cloudflare API密钥和电子邮件：${NC}"
+read -p "Cloudflare API密钥: " CF_API_KEY
+read -p "Cloudflare 邮箱: " CF_EMAIL
+
+# 配置Cloudflare的API密钥和电子邮件
+echo -e "${GREEN}配置Cloudflare的API密钥和电子邮件...${NC}"
+acme.sh --set-default-ca --server letsencrypt --dns dns_cf --accountemail $CF_EMAIL --accountkey $CF_API_KEY
+
+# 验证密钥和电子邮件的有效性
+if [ $? -ne 0 ]; then
+    echo -e "${GREEN}API密钥或电子邮件验证失败，请检查输入的信息。脚本将退出。${NC}"
+    exit 1
 fi
 
-# 输入API密钥和邮箱  
-read -p "请输入Cloudflare API密钥:" CF_Key
-read -p "请输入Cloudflare 邮箱:" CF_Email
+# 输入需要申请的域名
+echo -e "${GREEN}请输入需要申请的域名类型：${NC}"
+select domain_type in "主域名" "单域名" "泛域名"; do
+    case $domain_type in
+        "主域名")
+            read -p "请输入主域名: " main_domain
+            acme.sh --issue --dns dns_cf -d $main_domain --key-file /root/cert/$main_domain.key --fullchain-file /root/cert/$main_domain.cer --keylength ec-256 --force
+            ;;
+        "单域名")
+            read -p "请输入单域名: " single_domain
+            acme.sh --issue --dns dns_cf -d $single_domain --key-file /root/cert/$single_domain.key --fullchain-file /root/cert/$single_domain.cer --keylength ec-256 --force
+            ;;
+        "泛域名")
+            read -p "请输入泛域名: " wildcard_domain
+            acme.sh --issue --dns dns_cf -d "*.$wildcard_domain" --key-file /root/cert/$wildcard_domain.key --fullchain-file /root/cert/$wildcard_domain.cer --keylength ec-256 --force
+            ;;
+        *)
+            echo -e "${GREEN}无效的选择。脚本将退出。${NC}"
+            exit 1
+            ;;
+    esac
+    break
+done
 
-# 配置并验证Cloudflare API
-export CF_Key="$CF_Key"
-export CF_Email="$CF_Email"
-
-
-# 输入域名
-read -p "请输入要申请证书的主域名:" main_domain
-read -p "请输入要申请证书的泛域名:" sans_domain
-
-# 申请证书
-if ! acme.sh --issue --dns dns_cf -d $main_domain -d "*.$sans_domain" --keylength ec-256 --force; then
-  echo -e "\033[1;31m证书申请失败\033[0m"
-  exit 1
+# 检查证书路径并创建
+cert_path="/root/cert"
+if [ ! -d $cert_path ]; then
+    mkdir -p $cert_path
 fi
 
-echo -e "\033[1;32m证书申请成功\033[0m"
+# 复制证书到证书目录
+cp /root/.acme.sh/*.cer $cert_path/
+cp /root/.acme.sh/*.key $cert_path/
 
-# 检查证书目录是否存在
-if [ ! -d "/root/cert" ]; then
-  mkdir /root/cert
-fi
-
-# 拷贝证书并替换私钥文件名
-acme.sh --install-cert -d $main_domain --key-file "/root/cert/${main_domain}_.key" --fullchain-file /root/cert/fullchain.cer --ecc
-
-echo -e "\033[1;32m证书已保存到/root/cert目录\033[0m"
+echo -e "${GREEN}证书申请成功并已复制到目录$cert_path${NC}"
