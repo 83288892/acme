ec8679174ef1512ac8e711a819168b78656329ce
li <43067679+83288892@users.noreply.github.com>
2023-08-01 14:45:56 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -1,82 +1,45 @@
 #!/bin/bash
 
-# 设置绿色高亮字体
-echo -e "\033[1;32m"
+# 字体高亮 大绿色
+echo -e "\033[1;32m"  
 
-# 检查 acme.sh 是否已安装
-if [ ! -f "/root/.acme.sh/acme.sh" ]; then
-  echo -e "\033[1;32macme.sh 未安装,开始安装...\033[0m"
-
-  # 使用官方安装脚本安装 acme.sh
-  curl https://get.acme.sh | sh
-
-  if [ ! -f "/root/.acme.sh/acme.sh" ]; then
-    echo -e "\033[1;31macme.sh 安装失败,请重试!\033[0m"
-    exit 1
-  fi
-
-else
-  echo -e "\033[1;32macme.sh 已安装,开始申请证书...\033[0m"
+# 检查acme.sh和socat是否安装
+if ! command -v acme.sh >/dev/null 2>&1 || ! command -v socat >/dev/null 2>&1; then
+  # 如果没有安装,提示并安装
+  echo -e "\033[1;33macme.sh或socat未安装,正在安装...\033[0m"
+  apt install -y acme.sh socat 
 fi
 
-# 检查 socat 是否已安装
-if [ ! -f "/usr/bin/socat" ]; then
-  echo -e "\033[1;32msocat 未安装,开始安装...\033[0m"
-  
-  apt install socat -y
-  
-  if [ $? -ne 0 ]; then
-    echo -e "\033[1;31msocat 安装失败,请重试!\033[0m"
-    exit 1
-  fi
+# 输入API密钥和邮箱  
+read -p "请输入Cloudflare API密钥:" CF_Key
+read -p "请输入Cloudflare 邮箱:" CF_Email
 
-else
-  echo -e "\033[1;32msocat 已安装,继续...\033[0m"
-fi
-
-# 输入 API 信息并验证
-while :
-do 
-  # 读取并处理输入
-  echo -e "\033[1;32m请输入 Cloudflare API Key:\033[0m"
-  read -p "" CF_Key
-  echo -e "\033[1;32m请输入 Cloudflare 邮箱:\033[0m"
-  read -p "" CF_Email  
-
-  # 去除空格
-  CF_Key="${CF_Key//[[:space:]]/}"
-  CF_Email="${CF_Email//[[:space:]]/}"
-
-  if [ -z "$CF_Key" ] || [ -z "$CF_Email" ]; then
-    echo -e "\033[1;31mKey 或 Email 不能为空!\033[0m"
-  else
-    break
-  fi
-done
-
-# 配置并验证 API 可用性
-echo -e "\033[1;32m正在验证 Cloudflare API...\033[0m"
-if ! ~/.acme.sh/acme.sh --register-account -m $CF_Email --server letsencrypt --accountemail $CF_Email --accountkey $CF_Key; then
-  echo -e "\033[1;31mAPI 验证失败,请检查后重试!\033[0m"
+# 配置并验证Cloudflare API
+export CF_Key="$CF_Key"
+export CF_Email="$CF_Email"
+if ! acme.sh --issue --dns dns_cf -d example.com -k ec-256 --force; then
+  echo -e "\033[1;31mCloudflare API验证失败,请检查密钥和邮箱是否正确\033[0m"
   exit 1
-else
-  echo -e "\033[1;32mAPI 验证成功!\033[0m"
+fi  
+
+# 输入域名
+read -p "请输入要申请证书的主域名:" main_domain
+read -p "请输入要申请证书的泛域名:" sans_domain
+
+# 申请证书
+if ! acme.sh --issue --dns dns_cf -d $main_domain -d $sans_domain --keylength ec-256 --force; then
+  echo -e "\033[1;31m证书申请失败\033[0m"
+  exit 1
 fi
 
-# 读取并申请证书 
-echo -e "\033[1;32m请输入要申请证书的域名:\033[0m"
-read -p "" domain
-if ! ~/.acme.sh/acme.sh --issue --dns dns_cf -d $domain --keylength ec-256; then
-  echo -e "\033[1;31m证书申请失败,请重试!\033[0m"
-  exit 1 
-else
-  echo -e "\033[1;32m证书申请成功!\033[0m"  
+echo -e "\033[1;32m证书申请成功\033[0m"
+
+# 检查证书目录是否存在
+if [ ! -d "/root/cert" ]; then
+  mkdir /root/cert
 fi
 
-# 复制证书到目录
-cert_path="/root/cert"
-if [ ! -d "$cert_path" ]; then
-  mkdir "$cert_path"
-fi 
-~/.acme.sh/acme.sh --install-cert -d $domain --key-file $cert_path/$domain.key --cert-file $cert_path/fullchain.cer
-echo -e "\033[1;32m证书已复制到 $cert_path 目录!\033[0m"
+# 拷贝证书
+acme.sh --install-cert -d $main_domain --key-file /root/cert/private.key --fullchain-file /root/cert/fullchain.cer --ecc
+
+echo -e "\033[1;32m证书已保存到/root/cert目录\033[0m"
