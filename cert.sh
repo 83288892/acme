dd7434ba6d29a4a4cce3398fb68ddbb85dbb72da
li <43067679+83288892@users.noreply.github.com>
2023-08-01 12:10:11 +0800

    Update cert.sh

--- cert.sh
+++ cert.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+# 检查是否支持 ANSI 转义序列
+if [ -t 1 ]; then
+    red='\033[31m'
+    green='\033[32m'
+    reset='\033[0m'
+else
+    red=''
+    green=''
+    reset=''
+fi
+
 # 检查是否已安装 acme.sh 和 jq 工具
 if command -v acme.sh &> /dev/null && command -v jq &> /dev/null; then
-    echo -e "\033[32macme.sh 和 jq 工具已经安装，开始申请证书...\033[0m"
+    echo -e "${green}acme.sh 和 jq 工具已经安装，开始申请证书...${reset}"
 else
-    echo -e "\033[31macme.sh 或 jq 工具未安装。\033[0m"
+    echo -e "${red}acme.sh 或 jq 工具未安装。${reset}"
 
     # 提示用户选择是否安装缺少的工具
     echo "请选择操作："
@@ -18,17 +29,17 @@ else
                 break
                 ;;
             "退出脚本")
-                echo -e "\033[31m已退出脚本。\033[0m"
+                echo -e "${red}已退出脚本。${reset}"
                 exit 1
                 ;;
-            *) echo -e "\033[31m无效的选项，请重新选择。\033[0m" ;;
+            *) echo -e "${red}无效的选项，请重新选择。${reset}" ;;
         esac
     done
 fi
 
 # 设置绿色高亮输出函数
 function print_success() {
-    echo -e "\033[32m$1\033[0m"
+    echo -e "${green}$1${reset}"
 }
 
 # 获取 Cloudflare API 密钥和邮箱
@@ -40,7 +51,7 @@ print_success "正在验证密钥..."
 if ~/.acme.sh/acme.sh --dns dns_cf --accountemail "$cf_email" --registeraccount; then
     print_success "密钥验证成功！"
 else
-    echo -e "\033[31m密钥验证失败，请检查您的密钥和邮箱是否正确。\033[0m"
+    echo -e "${red}密钥验证失败，请检查您的密钥和邮箱是否正确。${reset}"
     exit 1
 fi
 
@@ -65,5 +76,5 @@ if [ $? -eq 0 ]; then
     ~/.acme.sh/acme.sh --install-cert -d "$domain" --cert-file "$cert_dir/$domain.cer" --key-file "$cert_dir/$domain.key" --fullchain-file "$cert_dir/fullchain.cer" --reloadcmd "echo 证书复制成功，存放路径：$cert_dir"
 
 else
-    echo -e "\033[31m证书申请失败，请检查您的域名是否正确，并确保您的 DNS 设置已经生效。\033[0m"
+    echo -e "${red}证书申请失败，请检查您的域名是否正确，并确保您的 DNS 设置已经生效。${reset}"
 fi
